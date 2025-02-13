20f92df80ac6022071eb914e68dab31797503716
SAYONE <31896368+DARKSAYONE@users.noreply.github.com>
2025-02-13 17:56:09 +0300

    Task one complete
    • Added a new class LabyrinthFruit with functionality to replace ball models with fruits.
    • Modified Level.cs to include a check for "fruit" levels.

---
+++ Assets/Scripts/LabyrinthFruit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrinthFruit : Labyrinth
+{
+    [SerializeField] private GameObject[] fruitPrefabs;
+
+    public void ReplaceBallModels(List<Ball> balls)
+    {
+        //Debug.Log("ReplaceBall ON");
+        foreach (var ball in balls)
+        {
+            var meshFilter = ball.GetComponent<MeshFilter>();
+            var meshRenderer = ball.GetComponent<MeshRenderer>();
+            if (meshFilter != null && meshRenderer != null && fruitPrefabs.Length > 0)
+            {
+                int randomIndex = Random.Range(0, fruitPrefabs.Length);
+                var fruitPrefab = fruitPrefabs[randomIndex];
+                var fruitMeshFilter = fruitPrefab.GetComponent<MeshFilter>();
+                var fruitMeshRenderer = fruitPrefab.GetComponent<MeshRenderer>();
+
+                if (fruitMeshFilter != null && fruitMeshRenderer != null)
+                {
+                    meshFilter.mesh = fruitMeshFilter.sharedMesh;
+                    meshRenderer.sharedMaterial = fruitMeshRenderer.sharedMaterial;
+                }
+            }
+        }
+    }
+}
+
