c1b633713108966678dacf85d3ffb9145a2bb642
SAYONE <31896368+DARKSAYONE@users.noreply.github.com>
2025-02-13 18:49:11 +0300

    Implemented key collection mechanics
    • Created LabyrinthKey class inheriting from Labyrinth and implementing functionality for key collection.
    
    • Implemented Key class that registers keys in the labyrinth class and handles interaction with balls.

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private bool isCollected = false;
+
+    private void Awake()
+    {
+        LabyrinthKey labyrinthKey = GetComponentInParent<LabyrinthKey>();
+        if (labyrinthKey != null)
+        {
+            labyrinthKey.RegisterKey(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isCollected && other.GetComponent<Ball>() != null)
+        {
+            isCollected = true;
+            LabyrinthKey.Instance.CollectKey(gameObject);
+            Debug.Log("Key trigger activated by Ball.");
+        }
+    }
+}
---
+++ Assets/Scripts/LabyrinthKey.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine.UI;
+
+public class LabyrinthKey : Labyrinth
+{
+    [SerializeField] private List<GameObject> keys = new List<GameObject>();
+    [SerializeField] private GameObject door;
+    // [SerializeField] private Text keyCounterText;
+
+    public int keysCollected = 0;
+    public int totalKeys;
+
+    public static LabyrinthKey Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        // UpdateKeyCounter();
+    }
+
+    public void RegisterKey(GameObject key)
+    {
+        keys.Add(key);
+        totalKeys = keys.Count;
+        // UpdateKeyCounter();
+    }
+
+    public void CollectKey(GameObject key)
+    {
+        keysCollected++;
+        keys.Remove(key);
+        Destroy(key);
+        Debug.Log($"Key collected. Total keys collected: {keysCollected}/{totalKeys}");
+        // UpdateKeyCounter();
+
+        if (keysCollected >= totalKeys)
+        {
+            OpenDoor();
+        }
+    }
+
+    // private void UpdateKeyCounter()
+    // {
+    //     keyCounterText.text = $"Keys: {keysCollected}/{totalKeys}";
+    // }
+
+    private void OpenDoor()
+    {
+        door.SetActive(false);
+        Debug.Log("Door opened.");
+    }
+}
